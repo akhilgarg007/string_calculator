29bb98615ffca06c814b084af770048ced70ef2d
Akhil Garg <akhilgargjpr@gmail.com>
2025-07-31 14:25:57 +0000

    4. Support different delimiters

--- string_calculator.py
+++ string_calculator.py
@@ -4,5 +4,14 @@ import re
 def add(numbers: str) -> int:
     if not numbers:
         return 0
-    parts = re.split(r",|\n", numbers)
+
+    delimiters = [",", "\n"]
+
+    if numbers.startswith("//"):
+        header, numbers = numbers.split('\n', 1)
+        custom_delim = header[2:]
+        delimiters.append(re.escape(custom_delim))
+
+    pattern = "|".join(delimiters)
+    parts = re.split(pattern, numbers)
     return sum(map(int, parts))
