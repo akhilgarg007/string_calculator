2b010899dc39a6853b29889f99bf437b8e3b71aa
Akhil Garg <akhilgargjpr@gmail.com>
2025-07-31 14:35:21 +0000

    8. Allow multiple delimiters like this: “//[delim1][delim2]\n”

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -33,6 +33,8 @@ class TestStringCalculator(unittest.TestCase):
     def test_multi_char_delimiter(self):
         self.assertEqual(add("//[***]\n1***2***3"), 6)
 
+    def test_multiple_delimiters(self):
+        self.assertEqual(add("//[*][%]\n1*2%3"), 6)
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(TestStringCalculator)
